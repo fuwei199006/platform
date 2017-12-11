06522923bcf77a4a6d0bf4b0bb1593a7dc727197
unknown <756091180@qq.com>
2017-12-11 22:37:57 +0800

    first

---
+++ web/src/main/java/org/pay/web/Controller/HelloController.java
@@ -0,0 +1,11 @@
+package org.pay.web.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String Welcome(){
+		return "welcome";
+	}
+}
