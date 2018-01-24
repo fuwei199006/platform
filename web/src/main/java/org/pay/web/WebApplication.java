4ae9a0d11effa30b883ca1525626c4eeb198464d
fuweiw_fu <w_fu@Ctrip.com>
2018-01-24 21:42:00 +0800

    fix

--- web/src/main/java/org/pay/web/WebApplication.java
+++ web/src/main/java/org/pay/web/WebApplication.java
@@ -1,21 +1,23 @@
 package org.pay.web;
 
-import ch.qos.logback.core.net.ssl.ConfigurableSSLServerSocketFactory;
 import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(scanBasePackages = "org.pay.web.controller")
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
 public class WebApplication extends SpringBootServletInitializer {
 
-//	public static void main(String[] args) {
-//		SpringApplication.run(WebApplication.class, args);
-//	}
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
