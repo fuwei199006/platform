9197f2125d2a9dc8bd0918f76c78a2d63ea2fc04
fuweiw_fu <w_fu@Ctrip.com>
2018-01-26 17:41:29 +0800

    fix

--- dal/src/test/java/test.java
+++ dal/src/test/java/test.java
@@ -1,8 +1,26 @@
 import org.junit.Test;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class test {
 	@Test
 	public void test(){
+		
 		System.out.println("1231");
+	}
+	
+	
+	@Test
+	public void testDb(){
+		
+		dbHelper db=new dbHelper("SELECT * FROM outlet_deliveryticket ");
+		try {
+			ResultSet set=db.pst.executeQuery();
+			set.next();
+			System.out.println(set.getString(5));
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
