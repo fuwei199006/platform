3555daafc504d17585471ad54569b19f81f87b6a
fuweiw_fu <w_fu@Ctrip.com>
2018-01-26 14:37:48 +0800

    fix

--- web/src/main/java/org/pay/web/WebApplication.java
+++ web/src/main/java/org/pay/web/WebApplication.java
@@ -4,20 +4,22 @@ import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
-
-@SpringBootApplication
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class}
+		,scanBasePackages = {"org.pay.web.controller"})
+@ServletComponentScan
 public class WebApplication extends SpringBootServletInitializer  {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebApplication.class, args);
-	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(WebApplication.class, args);
+//	}
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
