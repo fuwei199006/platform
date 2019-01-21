d246ba4bcf579acea98f52dfd23da3ba5def7f4b
fuweiw_fu <w_fu@Ctrip.com>
2019-01-21 09:34:52 +0800

    fix log

--- platform-common/src/main/java/com/pay/utils/ElasticUtils.java
+++ platform-common/src/main/java/com/pay/utils/ElasticUtils.java
@@ -5,18 +5,21 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.simple.SimpleLogger;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
+//import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 
 public class ElasticUtils {
 	public static void main(String[] args) {
 		try {
-			
-			@SuppressWarnings({"resource", "unchecked"}) TransportClient transportClient = new PreBuiltTransportClient(Settings.EMPTY).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9200));
+ 
+			@SuppressWarnings({"resource", "unchecked"})
+			TransportClient transportClient = new PreBuiltTransportClient(Settings.EMPTY).addTransportAddress(new TransportAddress(InetAddress.getByName("127.0.0.1"), 9200));
 			System.out.println(transportClient);
 			transportClient.close();
 		} catch (Exception ex) {
@@ -26,14 +29,14 @@ public class ElasticUtils {
 		
 	}
 	
-	private static TransportClient getClient() {
-		try{
-			TransportClient transportClient = new PreBuiltTransportClient(Settings.EMPTY).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9200));
-			return transportClient;
-		}catch (UnknownHostException ex){
-		return null;
+		private static TransportClient getClient() {
+			try{
+				TransportClient transportClient = new PreBuiltTransportClient(Settings.EMPTY).addTransportAddress(new TransportAddress(InetAddress.getByName("127.0.0.1"), 9200));
+				return transportClient;
+			}catch (UnknownHostException ex){
+			return null;
+			}
+
 		}
-	
-	}
 	
 }
